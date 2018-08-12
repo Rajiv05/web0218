fb585b71f2fd8e1794f26518325076507670143a
Rajiv NARANDAS <932400679@cit3-401-16m.acg.ac.nz>
2018-08-13 09:52:15 +1200

    first commit

---
+++ assets/myscriptt.js
@@ -0,0 +1,32 @@
+$(function(){
+    
+    console.log('bla');
+    
+    $('.nav').hide()   
+
+    $('.bars').on('click',function(){
+
+       $('.nav').slideToggle();
+
+    });
+
+
+
+});
+
+
+$(function(){
+    
+    console.log('bla');
+    
+    $('.bottom-nav').hide()   
+
+    $('.down').on('click',function(){
+
+       $('.bottom-nav').slideToggle();
+
+    });
+
+
+
+});
